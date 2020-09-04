0cfc4f8ab4a57bd63b316b6b85c6486480b7a35c
Sergio <sgonzalez.uade@gmail.com>
2020-09-04 13:34:50 -0300

    Se eliminan namespaces sin uso. Se agrega .gitignore

--- src/test/java/singlesignon/test/singleSignOnTest.java
+++ src/test/java/singlesignon/test/singleSignOnTest.java
@@ -8,20 +8,15 @@ import java.io.InputStreamReader;
 import java.util.Random;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
