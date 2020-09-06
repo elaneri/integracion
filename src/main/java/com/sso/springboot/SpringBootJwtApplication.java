4c2eea90fd043d153d04cca92999fd77fe05950a
Sergio <sgonzalez.uade@gmail.com>
2020-09-05 23:20:10 -0300

    prueba

--- src/main/java/com/sso/springboot/SpringBootJwtApplication.java
+++ src/main/java/com/sso/springboot/SpringBootJwtApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-
-
 @SpringBootApplication(scanBasePackages={
 "singlesignon.authorizationserver", "singlesignon.controller"})
 @EnableCaching
