0fe976196108ef370820de64da7e1b7667e43ed9
Sergio <sgonzalez.uade@gmail.com>
2020-09-07 00:01:28 -0300

    Se eliminan por ahora clases innecesarias

--- src/main/java/com/sso/springboot/Tenant/TenantDAO.java
+++
@@ -1,6 +0,0 @@
-package com.sso.springboot.Tenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TenantDAO extends JpaRepository<Tenant, Long> {
-
-}
--- src/main/java/com/sso/springboot/Tenant/TenantService.java
+++
@@ -1,9 +0,0 @@
-package com.sso.springboot.Tenant;
-
-import java.util.Optional;
-
-public interface TenantService {
-	
-	public Optional<Tenant> findById(Long id);
-	
-}
