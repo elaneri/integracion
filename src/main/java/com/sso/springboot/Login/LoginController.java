1574b040a38703bc678f3d825f65ab61af9bf96e
Sergio <sgonzalez.uade@gmail.com>
2020-09-13 02:03:05 -0300

    ...

--- src/main/java/com/sso/springboot/Login/LoginController.java
+++ src/main/java/com/sso/springboot/Login/LoginController.java
@@ -10,8 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
