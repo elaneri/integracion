6150f714aaea3e9e9058bf6612e08b1eddf3fa85
Sergio <sgonzalez.uade@gmail.com>
2020-09-30 02:41:25 -0300

    ...

--- src/main/java/com/sso/springboot/UserClaims/UserClaimsDAO.java
+++ src/main/java/com/sso/springboot/UserClaims/UserClaimsDAO.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.sso.springboot.Claims.Claim;
-import com.sso.springboot.Tenant.Tenant;
 import com.sso.springboot.Usuario.Usuario;
 
 
