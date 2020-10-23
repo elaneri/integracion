78791fb28571e003a37805eb7d26bcead97a82d8
Sergio <sgonzalez.sergiod@gmail.com>
2020-10-23 03:31:07 -0300

    Se establecen código de error 400 para las validaciones del usuario

--- src/main/java/com/sso/springboot/Validaciones/UsuarioHelper.java
+++ src/main/java/com/sso/springboot/Validaciones/UsuarioHelper.java
@@ -5,6 +5,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.regex.Pattern;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.sso.springboot.Usuario.AccionUsuario;
 import com.sso.springboot.Usuario.Usuario;
 
@@ -24,67 +27,68 @@ public class UsuarioHelper {
 		
 		//Validaciones de nombre y apellido
 		if(usuario.getNombre().trim().equals(""))
-			throw new Exception("El nombre del usuario no puede estar vacío");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El nombre del usuario no puede estar vacío");
 		
 		if(usuario.getNombre().trim().length() > 15)
-			throw new Exception("El nombre del usuario no puede contener más de 15 caracteres");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El nombre del usuario no puede contener más de 15 caracteres");
+			
 
 		if(usuario.getApellido().trim().equals(""))
-			throw new Exception("El apellido del usuario no puede estar vacío");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El apellido del usuario no puede estar vacío");
 		
 		if(usuario.getApellido().trim().length() > 15)
-			throw new Exception("El apellido del usuario no puede contener más de 15 caracteres");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El apellido del usuario no puede contener más de 15 caracteres");
 		
 		if (accion.equals(AccionUsuario.ALTA)) {
 			if(usuario.getUsuario().trim().equals(""))
-				throw new Exception("El usuario no puede estar vacío");
-			
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El usuario no puede estar vacío");
+				
 			if(usuario.getUsuario().trim().length() > 10)
-				throw new Exception("El usuario no puede contener más de 10 caracteres");
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El usuario no puede contener más de 10 caracteres");
 		}
 		
 		//Validaciones password	
 		if( usuario.getPassword().equals(""))
-			throw new Exception("La password del usuario no puede estar vacía");
-		
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"La password del usuario no puede estar vacía");
+					
 		if (!Pattern.matches(rg_digitos, usuario.getPassword())) {
-			throw new IllegalArgumentException ("El password debe tener al menos un número");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El password debe tener al menos un número");
 		}
 		
 		if (!Pattern.matches(rg_onelowercase, usuario.getPassword())) {
-			throw new Exception("El password debe tener al menos una minúscula");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El password debe tener al menos una minúscula");
 		}
 		
 		if (!Pattern.matches(rg_oneuppercase, usuario.getPassword())) {
-			throw new Exception("El password debe tener al menos una mayúscula");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El password debe tener al menos una mayúscula");
 		}
 		
 		if (!Pattern.matches(rg_specialchar, usuario.getPassword())) {
-			throw new Exception("El password debe tener al menos un caracter especial (@#$%!.)");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El password debe tener al menos un caracter especial (@#$%!.)");
 		}
 		
 		if (!Pattern.matches(rg_length, usuario.getPassword())) {
-			throw new Exception("El password debe tener al menos 8 caracteres y como máximo 15");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El password debe tener al menos 8 caracteres y como máximo 15");
 		}				
 		
 		//Validaciones email
 		if(usuario.getMail().trim().length() > 30)
-			throw new Exception("El mail del usuario no puede contener más de 30 caracteres");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El mail del usuario no puede contener más de 30 caracteres");
 		
 		if( !Pattern.matches(rg_email, usuario.getMail()))
-			throw new Exception("El email tiene un formato inválido");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El email tiene un formato inválido");
 		
 		//Validaciones tel
 		if( !Pattern.matches(rg_tel, usuario.getTelefono()))
-			throw new Exception("El teléfono tiene un formato inválido");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El teléfono tiene un formato inválido");
 		
 		if( usuario.getTelefono().trim().length() < 8 || usuario.getTelefono().trim().length() > 20)
-			throw new Exception("El teléfono debe contener entre 8 y 20 caracteres sin guiones u otro caracter");	
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"El teléfono debe contener entre 8 y 20 caracteres sin guiones u otro caracter");
 		
 		//Validaciones fecha
 		if( !Pattern.matches(rg_fecha, usuario.getFecha_nacimiento()))
-			throw new Exception("La fecha de nacimiento tiene un formato inválido");
-		
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"La fecha de nacimiento tiene un formato inválido");
+					
 		if( Pattern.matches(rg_fecha, usuario.getFecha_nacimiento())) {
 			SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
 			format.setLenient(false);
@@ -92,17 +96,17 @@ public class UsuarioHelper {
 			try {
 				date = format.parse(usuario.getFecha_nacimiento());
 			} catch (ParseException e) {
-				throw new Exception("La fecha de nacimiento es inválida");
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"La fecha de nacimiento es inválida");
 			}
 		}
 		
 		Date fechaActual = new Date();
 		if(convertirFechaAFormatoJapones(fechaActual).compareTo(usuario.getFecha_nacimiento()) < 0) {
-			throw new Exception("La fecha de nacimiento no puede ser mayor a la fecha actual");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"La fecha de nacimiento no puede ser mayor a la fecha actual");
 		}
 	
 		if (!usuario.getPropiedades().isNull() &&  !JSONUtils.isJSONValido(usuario.getPropiedades().toString())) {
-			throw new Exception("Las propiedades del usuario deben estar en formato JSON");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Las propiedades del usuario deben estar en formato JSON");
 		}
 	}
 	
